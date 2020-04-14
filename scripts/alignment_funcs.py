30ad6e874e31015ca440380967dc8cfd322cb15c
marykthompson <mkaythomp@gmail.com>
2020-04-14 16:31:55 +0100

    make alignment file even if only 1 sequence

--- scripts/alignment_funcs.py
+++ scripts/alignment_funcs.py
@@ -40,41 +40,31 @@ def column_from_residue_number(aln, id, res_no):
 def write_alignment(fasta, name, outname):
     '''
     Make an alignment and consensus sequence for seqs in the provided fasta
-    If you provide the outdir as the family outdir, then this will be distinguished from
-    the species specific consensus by being one directory up.
     '''
-    #Check if only 1 sequence in the fasta file. If so, then just write this sequence
-    with open(fasta, 'r') as f:
-        num_seqs = len([1 for line in f if line.startswith(">")])
+    subprocess.run('mafft --auto --clustalout %s > %s.clustal' % (fasta, outname), shell = True)
+    alignment = AlignIO.read('%s.clustal' % outname, 'clustal')
+    info = AlignInfo.SummaryInfo(alignment)
+    #Get the sequence with alignment gaps or mismatches turned into Ns, but with the ends filled in with any sequence that covers it
+    # ungapped cons will not take gaps into acount, we can get the start and end sequence from here after applying the gapped_cons
+    ungapped_cons = SeqRecord(info.dumb_consensus(threshold = 1, ambiguous = 'N'), id = '%s_consensus' % name, description = "")
 
-    if num_seqs == 1:
-        record = SeqIO.parse(fasta, 'fasta')
-        SeqIO.write(record, outname, 'fasta')
-    else:
-        subprocess.run('mafft --auto --clustalout %s > %s.clustal' % (fasta, outname), shell = True)
-        alignment = AlignIO.read('%s.clustal' % outname, 'clustal')
-        info = AlignInfo.SummaryInfo(alignment)
-        #Get the sequence with alignment gaps or mismatches turned into Ns, but with the ends filled in with any sequence that covers it
-        # ungapped cons will not take gaps into acount, we can get the start and end sequence from here after applying the gapped_cons
-        ungapped_cons = SeqRecord(info.dumb_consensus(threshold = 1, ambiguous = 'N'), id = '%s_consensus' % name, description = "")
+    #'require multiple' means it will place an N if only one sequence unaligned
+    # this is the desired behavior for internal gaps but not for the end gaps -- because some of the sequences are incomplete
+    gapped_cons = SeqRecord(info.gap_consensus(threshold = 1, ambiguous = 'N', require_multiple = 1), id = '%s_consensus' % name, description = "")
 
-        #'require multiple' means it will place an N if only one sequence unaligned
-        # this is the desired behavior for internal gaps but not for the end gaps -- because some of the sequences are incomplete
-        gapped_cons = SeqRecord(info.gap_consensus(threshold = 1, ambiguous = 'N', require_multiple = 1), id = '%s_consensus' % name, description = "")
+    gapped_len = len(gapped_cons.seq)
+    #get the indices where the gapped alignment should be used
+    left_strip = gapped_cons.seq.lstrip('N')
+    left_start = gapped_len - len(left_strip)
 
-        gapped_len = len(gapped_cons.seq)
-        #get the indices where the gapped alignment should be used
-        left_strip = gapped_cons.seq.lstrip('N')
-        left_start = gapped_len - len(left_strip)
+    right_strip = gapped_cons.seq.rstrip('N')
+    right_start = len(right_strip)
 
-        right_strip = gapped_cons.seq.rstrip('N')
-        right_start = len(right_strip)
+    left_seq = ungapped_cons.seq[0:left_start]
+    right_seq = ungapped_cons.seq[right_start:]
+    middle_seq = gapped_cons.seq[left_start: right_start]
 
-        left_seq = ungapped_cons.seq[0:left_start]
-        right_seq = ungapped_cons.seq[right_start:]
-        middle_seq = gapped_cons.seq[left_start: right_start]
-
-        whole_seq = left_seq + middle_seq + right_seq
-        record = SeqRecord(whole_seq, id = name, description = '')
-        SeqIO.write(record, outname, 'fasta')
-        return alignment
+    whole_seq = left_seq + middle_seq + right_seq
+    record = SeqRecord(whole_seq, id = name, description = '')
+    SeqIO.write(record, outname, 'fasta')
+    return alignment
